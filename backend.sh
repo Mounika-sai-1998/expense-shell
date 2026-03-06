454e92a809c5b1ddf2af9a95bc10450e770e4964
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 15:35:05 +0530

    expense -shell

--- backend.sh
+++ backend.sh
@@ -58,6 +58,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 Validate $? "downlading backend file"
 
 cd /app
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 Validate $? "Unzipping backend file"
 
