e408b8f2fc9fa8c882a449e65614ed260846b46b
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 11:59:53 +0530

    expense -shell

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
+TIMESTAMP=(date +%F-%H-%M-%S)
+SCRIPT_NAME=(echo $0 | cut -d  "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+Validate(){
+    if [ $1 -eq 0 ]
+    then
+         echo  -e "$2  $G SUCCESS......$N"
+    else
+        echo "$2  $R failure.......$N"
+    fi
+}
 if [ $USERID -ne 0 ]
 then
      echo "please run with a super user"
+     exit 1
 else  
-     echo "is a super user"
+     echo " It is a super user"
 fi
+
+dnf install mysql-server -y
+VALIDATE $@  "insallation of mysql server"
