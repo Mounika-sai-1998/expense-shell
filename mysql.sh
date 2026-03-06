abec52714e7ea42b8d90ecf7a6db3658719748cb
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 13:52:06 +0530

    expense -shell

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME=$(echo $0 | cut -d  "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 echo "ENTER YOUR PASSWORD :"
-read mysql_root_password
+read -s mysql_root_password
 
 R="\e[31m"
 G="\e[32m"
