4ef2e10b1fccdf803115ab35e8f8efe5014764dd
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 13:26:00 +0530

    expense -shell

--- mysql-practice.sh
+++ mysql-practice.sh
@@ -9,8 +9,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-#echo "Please enter DB password:"
-#read -s mysql_root_password
+echo "Please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -44,7 +44,7 @@ VALIDATE $? "Starting MySQL Server"
 #VALIDATE $? "Setting up root password"
 
 Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.mounikasai.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
