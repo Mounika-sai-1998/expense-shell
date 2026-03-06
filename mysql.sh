95ecb28578279892f4f9496d18a847e61a1c92c7
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 13:02:34 +0530

    expense -shell

--- mysql.sh
+++ mysql.sh
@@ -16,8 +16,9 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 is $R FAILURE...$N"
+        exit 1
     else
-        echo -e "$2 is $G SUCCESS.....$N"
+        echo -e "$2 is $G SUCCESS....$N"
     fi
 }
 if [ $USERID -ne 0 ]
