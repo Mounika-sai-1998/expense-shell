bae13fca26f1ccb254e50c719010b16cfce72fc8
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 12:21:15 +0530

    expense -shell

--- mysql.sh
+++ mysql.sh
@@ -15,9 +15,9 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "$2 is $G sucesss...$N"
+        echo -e "$2 is $G SUCCESS...$N"
     else
-        echo -e "$2 is $R failure....$N"
+        echo -e "$2 is $R FAILURE....$N"
     fi
 }
 if [ $USERID -ne 0 ]
@@ -29,4 +29,4 @@ else
 fi
 
 dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $@  "insallation of mysql server"
+VALIDATE $?  "insallation of mysql server"
